1979d17e0dd5c6d7ec485ad8937bd846589896f7
한태규 <gksxorb147@gmail.com>
2022-02-12 01:13:14 +0900

    - django rest framework static file 업로드

--- microdomains/settings.py
+++ microdomains/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_URL = 'http://34.64.175.9/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
