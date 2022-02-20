1066a685ba17bd4fd5b5adca6d443dee9a4552ed
한태규 <gksxorb147@gmail.com>
2022-02-20 20:26:31 +0900

    Fix : User 주소 데이터 추가(#008)
    address 데이터 추가
    char 100
    
    해결: close #008

--- user/models.py
+++ user/models.py
@@ -8,6 +8,7 @@ class User(models.Model):
     app_name = models.CharField(max_length=40)
     latitude = models.FloatField(db_index=True) # 위도 ex): 41.40338
     longitude = models.FloatField(db_index=True) # 경도 ex): 2.17403
+    address = models.CharField(max_length=100, default="82, Eunbong-ro, Namdong-gu, Incheon, Republic of Korea")
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
