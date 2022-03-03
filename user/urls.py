f5ac0179cfd84cee157ba4bf004cb7314768b9f4
한태규 <gksxorb147@gmail.com>
2022-03-03 11:48:56 +0900

    Fix : User 회원 가입 수정  (#019)
    - /user/login > /user/join 과 동일
    
    해결: close #019

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from .views import (
-    UserJoinAPIView, UserDeleteAPIView,
-    UserLoginAPIView
+    UserJoinAPIView, UserDeleteAPIView
 )
 
 urlpatterns = [
     path("join", UserJoinAPIView.as_view()),
     path("delete", UserDeleteAPIView.as_view()),
-    path("login", UserLoginAPIView.as_view()),
+    path("login", UserJoinAPIView.as_view()),
 ]
